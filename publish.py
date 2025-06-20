58b57c587825c4eef2fd401437a4a82628cfbc51
Markus Martens <markus.martens@gmx.net>
2025-06-20 21:54:23 +0200

    refactor

--- publish.py
+++ publish.py
@@ -7,11 +7,14 @@ load_dotenv()
 MQTT_USER = os.getenv("USER")
 MQTT_PASS = os.getenv("PASS")
 
+
+# noinspection PyUnusedLocal
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
+
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.username_pw_set(MQTT_USER, MQTT_PASS)
 mqttc.connect("xerxes.fritz.box", 1883, 60)
-mqttc.publish('mm/test','Hello World')
+mqttc.publish('mm/test', 'Hello World')
--- subscribe.py
+++ subscribe.py
@@ -7,12 +7,18 @@ load_dotenv()
 MQTT_USER = os.getenv("USER")
 MQTT_PASS = os.getenv("PASS")
 
+
+# noinspection PyUnusedLocal
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    client.subscribe("mm/test") # Subscribing in on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
+    client.subscribe(
+        "mm/test")  # Subscribing in on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
 
+
+# noinspection PyUnusedLocal
 def on_message(client, userdata, msg):
     print(f"Recieved topic '{msg.topic}' with payload '{msg.payload}'")
+
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
